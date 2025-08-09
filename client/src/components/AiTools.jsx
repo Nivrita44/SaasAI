8780e09e61c716ed1f3c5cac8bdb90578d82ef21
Nivrita44 <nivritamunib357@gmail.com>
2025-08-09 19:23:09 +0600

    home page fully done , dashboard ,sidebar done

--- client/src/components/AiTools.jsx
+++ client/src/components/AiTools.jsx
@@ -19,7 +19,9 @@ const AiTools = () => {
         {AiToolsData.map((tool,index)=>(
             <div key={index} className='p-8 m-4 max-w-xs rounded-lg bg-[#FDFDFE] 
             shadow-lg border border-gray-100 hover:translate-y-1 transition-all duration-300 cursor-pointer' onClick={() => user && navigate(tool.path)}>
-            <tool.Icon className='w-12 h-12 p-3 text-white rounded-xl' style={{background : `linear-gradient(to bottom, ${tool.bg.from}, ${tool.bg.to})`}}/>
+            <div className='w-12 h-12 p-3 text-white rounded-xl' style={{background : `linear-gradient(to bottom, ${tool.bg.from}, ${tool.bg.to})`}}>
+            <tool.Icon className='w-full h-full text-white' />
+            </div>
             <h3 className='mt-6 mb-3 text-lg font-semibold'>{tool.title}</h3>
             <p className='text-gray-400 text-sm max-w-[95%]'>
                 {tool.description}
