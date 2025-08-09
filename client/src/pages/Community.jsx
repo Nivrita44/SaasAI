7e600f1a9affc9742ff7c99b5094284f78840af9
Nivrita44 <nivritamunib357@gmail.com>
2025-08-10 00:25:11 +0600

    all necessary pages are created

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useUser } from '@clerk/clerk-react';
+import {dummyPunlishedCreationData} from '../assets/assets'
 
 const Community = () => {
+
+  const [creations,setCreations] = useState([]);
+  const {user} = useUser()
+
+  const fetchCreations = async () => {
+    setCreations(dummyPunlishedCreationData);
+  }
+
   return (
-    <div>
-      <h1>Community</h1>
+    <div className='flex-1 h-full flex flex-col gap-4 p-6'>
+      Creations
+      <div className='bg-white h-full w-full rounded-xl overflow-y-scoll'>
+
+      </div>
     </div>
   )
 }
